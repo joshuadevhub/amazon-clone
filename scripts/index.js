ff93b2fbcdfb95e95a69765d930d646dcdc8b01b
joshuadevhub <elemide.j.dev@gmail.com>
2025-08-11 21:57:55 +0100

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
 const dropdownMenu = document.getElementById("dropdown-menu");
 const dropdownContent = document.getElementById("dropdown-content");
 const dropdownClose = document.getElementById("dropdown-close");
+const dropdownButton = document.getElementById("dropdown-btn");
+const dropdownContainer = document.querySelector("#dropdown-text");
 
 dropdownMenu.addEventListener("click", () => {
   dropdownContent.classList.toggle("show");
-});
 
-dropdownClose.addEventListener("click", () => {
-  dropdownContent.classList.add("dropdown-content");
-})
+  if (dropdownContent.classList.contains("show")) {
+    dropdownButton.style.transform = "rotate(180deg)";
+    dropdownContainer.innerText = `See Less`;
+  } else {
+    dropdownButton.style.transform = "rotate(0deg)";
+    dropdownContainer.innerText = `See More`;
+  }
+});
