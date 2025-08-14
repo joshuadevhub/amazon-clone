194377396286690f22ce10eb10bc7199616be5d9
joshuadevhub <elemide.j.dev@gmail.com>
2025-08-14 11:58:51 +0100

    Almost done

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// JavaScript For The Dropdown Menu
 const dropdownMenu = document.getElementById("dropdown-menu");
 const dropdownContent = document.getElementById("dropdown-content");
 const dropdownClose = document.getElementById("dropdown-close");
@@ -16,6 +17,7 @@ dropdownMenu.addEventListener("click", () => {
   }
 });
 
+// JavaScript For The Open and Close Menu
 const hamBugger = document.getElementById("menu-icon");
 const sideBar = document.getElementById("side-bar");
 const closeIcon = document.getElementById("close-icon");
@@ -27,3 +29,73 @@ hamBugger.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
   sideBar.style.display = "none";
 })
+
+// JavaScript For The First Card Container
+
+const products = [
+  {
+    productTitle: "Score The Best PCs and Accessories",
+    productImg: "images/headphone-original.webp",
+    productDesc: "Laptop",
+    linkDesc: "See More",
+  },
+
+  {
+    productTitle: "Score The Best PCs and Accessories",
+    productImg: "images/laptop-smal-original.webp",
+    productDesc: "Laptop",
+    linkDesc: "See More",
+  },
+
+  {
+    productTitle: "Score The Best PCs and Accessories",
+    productImg: "images/headphone-original.webp",
+    productDesc: "Laptop",
+    linkDesc: "See More",
+  },
+
+  {
+    productTitle: "Score The Best PCs and Accessories",
+    productImg: "images/laptop-smal-original.webp",
+    productDesc: "Laptop",
+    linkDesc: "See More",
+  },
+]
+createCard(products);
+function createCard(e) {
+  const cardContainer = document.querySelector(".card-container");
+  cardContainer.innerHTML = '';
+
+  const title = document.createElement("h4");
+
+  const cardContents = document.createElement("div");
+  cardContents.classList.add("card-contents");
+
+  const link = document.createElement("p");
+
+  e.forEach(product => {
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+
+    const image = document.createElement("img");
+    const desc = document.createElement("p");
+
+    image.setAttribute("src", product.productImg);
+    image.setAttribute("loading", "lazy");
+    image.setAttribute("alt", `${product.productDesc} Image`);
+
+    title.textContent = product.productTitle;
+    desc.textContent = product.productDesc;
+    link.textContent = product.linkDesc;
+
+    cardContainer.appendChild(title);
+
+    cardContent.appendChild(image);
+    cardContent.appendChild(desc);
+
+    cardContents.appendChild(cardContent);
+    cardContainer.appendChild(cardContents);
+    cardContainer.appendChild(link);
+  });
+}
