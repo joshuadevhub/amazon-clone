ad65dd07d06b75372868c7a8d7365c9285232f62
joshuadevhub <elemide.j.dev@gmail.com>
2025-08-24 23:13:50 +0100

    Seperated The Each Html Page With It Own Js And Having a Main Js

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // JavaScript For The Open and Close Menu
+  const hamBugger = document.getElementById("menu-icon");
+  const sideBar = document.getElementById("side-bar");
+  const closeIcon = document.getElementById("close-icon");
+
+  hamBugger.addEventListener("click", () => {
+    sideBar.style.display = "block";
+  });
+
+  closeIcon.addEventListener("click", () => {
+    sideBar.style.display = "none";
+  });
+
+  // JavaScript For The Dropdown Menu
+  const dropdownMenu = document.getElementById("dropdown-menu");
+  const dropdownContent = document.getElementById("dropdown-content");
+  const dropdownClose = document.getElementById("dropdown-close");
+  const dropdownButton = document.getElementById("dropdown-btn");
+  const dropdownContainer = document.querySelector("#dropdown-text");
+
+  dropdownMenu.addEventListener("click", () => {
+    dropdownContent.classList.toggle("show");
+
+    if (dropdownContent.classList.contains("show")) {
+      dropdownButton.style.transform = "rotate(180deg)";
+      dropdownContainer.innerText = `See Less`;
+    } else {
+      dropdownButton.style.transform = "rotate(0deg)";
+      dropdownContainer.innerText = `See All`;
+    }
+  });
+});
