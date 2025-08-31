bb17452cd66ec7a92a65264e283039964d88979c
joshuadevhub <elemide.j.dev@gmail.com>
2025-08-31 13:22:26 +0100

    Added Some Changes

--- scripts/create-account.js
+++ scripts/create-account.js
@@ -5,11 +5,67 @@ document.addEventListener("DOMContentLoaded", () => {
   const personalEmail = document.querySelector("#personalemail");
   const personalPassword = document.querySelector("#personalpassword");
 
-  form1.addEventListener("click", () => {
+  form1.addEventListener("click", (e) => {
     e.preventDefault();
 
     showResult();
   });
+
+  personalFirstName.addEventListener("input", showResult);
+  personalLastName.addEventListener("input", showResult);
+  personalEmail.addEventListener("input", showResult);
+  personalPassword.addEventListener("input", showResult);
+
+
+  function showResult() {
+    // Get The Values
+    const personalFirstNameValue = personalFirstName.value.trim();
+    const personalLastNameValue = personalLastName.value.trim();
+    const personalEmailValue = personalEmail.value.trim();
+    const personalPasswordValue = personalPassword.value.trim();
+
+    if (personalFirstNameValue === '') {
+      setErrorFor(personalFirstName, "This Field Cannot Be Empty!");
+    } else {
+      setSuccessFor(personalFirstName);
+    }
+
+    if (personalLastNameValue === '') {
+      setErrorFor(personalLastName, "This Field Cannot Be Empty!");
+    } else {
+      setSuccessFor(personalLastName);
+    }
+
+    if (personalEmailValue === '') {
+      setErrorFor(personalEmail, "This Field Cannot Be Empty!");
+    } else {
+      setSuccessFor(personalEmail);
+    }
+
+    if (personalPasswordValue === '') {
+      setErrorFor(personalPassword, "This Field Cannot Be Empty!");
+    } else {
+      setSuccessFor(personalPassword);
+    }
+  }
+
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const errorMessage = formControl.querySelector(".error-message");
+    errorMessage.textContent = message;
+
+    formControl.classList.add("error");
+    formControl.classList.remove("success");
+  }
+
+  function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    const errorMessage = formControl.querySelector(".error-message");
+    errorMessage.textContent = '';
+
+    formControl.classList.add("success");
+    formControl.classList.remove("error");
+  }
 
 
   const personalButton = document.querySelector("#personal-button");
